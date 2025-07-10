d001a84ae5ee595a471fbaa3ef32559a196351d4
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2025-07-10 11:45:28 +0000

    day 3 done

---
+++ assignment day 3/q2.cpp
@@ -0,0 +1,8 @@
+// What’ll be the output of the following programs :
+// int main() {
+// int x = 10, y = 5;
+// int exp1 = (y * (x / y + x / y)); int exp2 = (y * x / y + y * x / y); cout << exp1 << " ";
+// cout << exp2 << "\n";
+// }
+
+output will be: 20 20
---
+++ assignment day 3/q3.cpp
@@ -0,0 +1,12 @@
+// What’ll be the output of the following programs :
+// int main() {
+// int x = 200, y = 50, z = 100; if(x > y && y > z){
+// cout << "Hello \n";
+// }
+// if(z > y && z < x){ cout << "C++ \n";
+// }
+// if((y+200) < x && (y+150) < z){ cout << "Hello C++ \n";
+// }
+// }
+
+output will be: “C++”
