592d0da9b00d0ef90d12a753d3f68621347d2fa1
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2025-07-12 12:28:08 +0000

    q4.cpp

---
+++ assignment 5/q4.cpp
@@ -0,0 +1,26 @@
+// Question 4 : For a positive N , WAP that prints all the prime numbers from 2 to N. (Assume N >= 2)
+
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int N = 15;
+    for (int i = 2; i <= N; i++)
+    {
+        int curr = i; // current number to check for 
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (curr % j == 0)
+            {
+                isPrime = false;
+            }
+        }
+        if (isPrime)
+        {
+            cout << curr << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
