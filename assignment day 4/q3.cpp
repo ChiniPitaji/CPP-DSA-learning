b5268d1b6db27e01f30f970083cde12b2e5a86ac
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2025-07-12 12:04:22 +0000

    q3 and q4 day4 done

---
+++ assignment day 4/q3.cpp
@@ -0,0 +1,9 @@
+// Question 3 : What will be the value of x & y in the following program:
+// int main() {
+// int a = 63, b = 36;
+// bool x = (a < b) ? true : false; int y = (a > b) ? a : b;
+// cout << x << "," << y << endl; return 0;
+// }
+
+
+answer will be :x = 0(false) & y = 63
---
+++ assignment day 4/q4.cpp
@@ -0,0 +1,11 @@
+// Question 4 : What’ll be the output of the program:
+// int main() { int a = 5;
+// if (++a*5 <= 25) {
+// cout<<"Hello\n";
+// } else {
+// cout<<"Bye\n";
+// }
+// return 0;
+// }
+
+answer will be: “Bye”.
