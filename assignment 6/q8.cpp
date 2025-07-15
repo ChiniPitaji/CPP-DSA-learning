e18283ecd9ce722d8006dc445d08d22cecc786e4
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2025-07-15 12:02:09 +0000

    floyd's triangle done

---
+++ assignment 6/q8.cpp
@@ -0,0 +1,24 @@
+// print floyd's triangle
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n = 5;
+    int num = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num++ << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
