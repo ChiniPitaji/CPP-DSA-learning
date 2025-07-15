283fd72fedad5718583c3fad1ee25f895ad56f6f
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2025-07-15 12:32:03 +0000

    butterfly pattern done

--- assignment 6/q10.cpp
+++ assignment 6/q10.cpp
@@ -8,30 +8,45 @@
 // * *         * *
 // *             *
 
-
 #include <iostream>
 using namespace std;
-int main(int argc, char const *argv[])
+int main()
 {
-    int n=4;
-    //upper half pattern
-    for (int i = 1; i <=n; i++)
+    int n = 4;
+    // upper half pattern
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <=i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout<<"*";
+            cout << "*";
         }
-        for (int j = 1; j <=2*(n-i); j++)
+        for (int j = 1; j <= 2 * (n - i); j++)
         {
-            cout<<" ";
+            cout << " ";
         }
-        for (int j = 1; j <=i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout<<"*";
+            cout << "*";
         }
-        cout<<endl;
-        
+        cout << endl;
     }
-    
+
+    //lower half pattern
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
