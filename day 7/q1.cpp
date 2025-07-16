392d7e63ec7df229edbcaa515d5dc3c098651346
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2025-07-16 11:04:21 +0000

    prduct finding

---
+++ day 7/q1.cpp
@@ -0,0 +1,12 @@
+// waf to find the product of two numbers- a & b 
+#include <iostream>
+using namespace std;
+
+int prod(int a, int b){
+    return a*b;
+}
+int main()
+{
+    cout<<prod(8,10)<<endl;
+    return 0;
+}
